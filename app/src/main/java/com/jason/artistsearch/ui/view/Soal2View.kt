12872081856b2ef9329d2d4c8fe082b9c739cae1
DkXenos <superjason101010@gmail.com>
2025-11-01 20:28:28 +0700

    init

---
+++ app/src/main/java/com/jason/artistsearch/ui/view/Soal2View.kt
@@ -0,0 +1,16 @@
+package com.jason.artistsearch.ui.view
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Soal2View(){
+    Text("Hello World")
+}
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun Soal2Preview(){
+    Soal2View()
+}
